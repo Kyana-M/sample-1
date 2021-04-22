8a61ae8d7af5dedbb6c3c678e33aa654f32bbb33
Kyana Mohammadi <80617434+Kyana-M@users.noreply.github.com>
2021-04-21 22:41:24 -0400

    Add files via upload

---
+++ ex-02-03.py
@@ -0,0 +1,4 @@
+#Write a program to prompt the user for hours and rate per hour using input to compute gross pay.
+hrs = input("Enter Hours:")
+rate= input('rate per hours:')
+print('Pay:',float(hrs)*float(rate))
